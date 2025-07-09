907405227b075cea2dd36774d72e2067543b2bd2
Fariz Ammar <f.ammarsyq11@gmail.com>
2025-07-09 18:25:52 +0700

    Testing branch

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Coba di rubah dlu biar kita dapat memastikan branch
           </li>
         </ol>
 
